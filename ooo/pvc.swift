134481981ac8ff2348bf2b632598ecad5c72c72e
alaa <alaa2amz@gmail.com>
2017-11-28 17:06:39 +0200

    Initial commit

--- ooo/pvc.swift
+++ ooo/pvc.swift
@@ -9,31 +9,68 @@
 import UIKit
 
 class pvc: UIPageViewController,UIPageViewControllerDataSource {
-    let p = UIViewController()
-    let g = UIViewController()
-    let b = UIViewController()
-    let o = UIViewController()
-    let m = UIViewController()
-    let vc : [ViewController] = []
+    var p = UIViewController()
+    var g = UIViewController()
+    var b = UIViewController()
+    var o = UIViewController()
+    var m = UIViewController()
+    var vc : [UIViewController] = []
+    var names = ["p","g","b","o","m"]
+    var index : Int = 0
+    var count : Int = 0
     func getVCs(ids: [String]) -> [UIViewController]
     {
+        var vcArray = [UIViewController]()
         for id in ids {
-      
+      let sb = UIStoryboard(name: "Main", bundle: nil)
+            let vc = sb.instantiateViewController(withIdentifier: id)
+            vcArray.append(vc)
+            
         }
+        return vcArray
+    }
+    func next( count:inout Int ,index:inout Int)
+    {
+       print("index before")
+         print(index)
+       
+        index += 1
+        if index > count-1 { index = 0 }
+       
+        print("index after")
+        print(index)
+        //if index == count { index = 0
+             //} else {  index += 1 }
+    }
+    
+    func previous( count:inout Int,index:inout Int)
+    {
+       index -= 1
+        if index < 0 { index = count-1 }
+        
+        //if index < 0 { index = count
+        //} else {  index -= 1 }
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        <#code#>
+        previous(count: &count, index: &index)
+        return vc[index]
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        <#code#>
+        next(count: &count, index: &index)
+        return vc[index]
     }
     
 
     override func viewDidLoad() {
+    
         super.viewDidLoad()
-
+        vc = getVCs(ids:names)
+          count = vc.count
+        setViewControllers([vc.first!], direction: .forward, animated: true, completion: nil)
+  dataSource = self
+        
         // Do any additional setup after loading the view.
     }
 
